cb4f31564e4981ff1156d31878574ea0d82ca0d2
DanielPulidoSC <danielpulidosc@gmail.com>
2021-01-12 19:14:40 +0100

    refinant

--- preguntes.R
+++ preguntes.R
@@ -4,3 +4,9 @@ sort(table(incidents.data["industry"]))
 sort(table(incidents.data["org"]))
 #Obtenim la columna mitre_attack
 attacks <- dplyr::select(.data = incidents.data, mitre_attack)
+#o d'aquesta altra manera
+mitre.attack <- incidents.data$mitre_attack
+#Finalment, fotem la llista de dataframes en un sol dataframe
+mitre.final = data.table::rbindlist(mitre.attack)
+#o d'aquesta altra manera
+mitre.final <- dplyr::bind_rows(mitre.attack)
